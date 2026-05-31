b4d2bd6a4d0171a1acacd7e76c12aafb7cc69c1b
Pramit <primeshipbas@gmail.com>
2026-05-31 17:02:24 +0545

    Initial commit

---
+++ src/main/java/org/example/learning_jpa/models/Author.java
@@ -0,0 +1,14 @@
+package org.example.learning_jpa.models;
+
+public class Author {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private int age;
+}
