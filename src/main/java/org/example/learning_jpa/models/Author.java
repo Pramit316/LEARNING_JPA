5fbbb1371f22a55b20c212fcf8f4debd4cf1f94a
Pramit <primeshipbas@gmail.com>
2026-06-01 07:39:26 +0545

    explore column annocation

--- src/main/java/org/example/learning_jpa/models/Author.java
+++ src/main/java/org/example/learning_jpa/models/Author.java
@@ -3,9 +3,12 @@ package org.example.learning_jpa.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @Entity
+
 public class Author {
 
     @Id
@@ -19,21 +22,44 @@ public class Author {
 //            allocationSize = 1
 //    )
 
-//    @TableGenerator(
-//            name="author_id_gen",
-//            table="id_generator",
-//            pkColumnName = "id_name",
-//            valueColumnName = "id_value",
-//            allocationSize = 1
-//    )
+    @TableGenerator(
+            name="author_id_gen",
+            table="id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
 
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
 
     private String lastName;
 
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
+    @Column(
+            nullable = false
+    )
     private int age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
 }
