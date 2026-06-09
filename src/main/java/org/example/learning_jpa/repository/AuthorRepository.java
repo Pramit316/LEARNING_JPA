5cf83de7d3d7d6ac6451e9167f50e995d43f9fe8
Pramit <primeshipbas@gmail.com>
2026-06-09 20:33:21 +0545

    exploring derived queries

--- src/main/java/org/example/learning_jpa/repository/AuthorRepository.java
+++ src/main/java/org/example/learning_jpa/repository/AuthorRepository.java
@@ -1,8 +1,26 @@
 package org.example.learning_jpa.repository;
 
 import org.example.learning_jpa.models.Author;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
+import java.util.List;
 
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    List<Author> findAllByFirstName(String fn);
+
+    //SELECT * FROM author WHERE first_name = ''
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    //SELECT * FROM author WHERE first_name like '%al%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    //SELECT * FROM author WHERE first_name like 'al%'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String firstName);
+
+    //SELECT * FROM author WHERE first_name like '%al'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String firstName);
+
+    //SELECT * FROM author WHERE first_name in ('pra', 'mit', 'god')
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
 }
